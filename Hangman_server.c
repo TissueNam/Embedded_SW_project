f26abfdb62d5b0980cd1778ea6b7139dadcc3f7c
Hyun Cheol Woong <hcw2155@naver.com>
2018-12-05 19:37:51 -0800

    Hangman_server_1.1

--- Hangman_server.c
+++ Hangman_server.c
@@ -12,6 +12,36 @@
 
 #define HINT 3
 
+int main(int argc, char *argv[])
+{
+    int serv_sock, clnt_sock;
+    char message[BUF_SIZE];
+    int str_len;
+    
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+ 
+    struct epoll_event event;
+    struct epoll_event *events;
+    int epfd;
+    int event_cnt, i;
+ 
+    int user_cnt=0;        //user count
+    int user[2]={0};    //user information = [ socket ]
+    int game_flag=0;    // Play game or not
+    int challenger[2]={0};    //challenger [0]=socket [1]=flag
+    int examiner[2]={0};    //examiner [0]=socket [1]=flag
+    char word[BUF_SIZE];    //answer
+    char question[BUF_SIZE];    //challenger's word
+    int hint_cnt, hang_cnt, j, iscorrect, iscontinue;
+    
+    if(argc!=2) {
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+}
+
 void error_handling(char *message)
 {
     fputs(message, stderr);
